5bfa7701d36ffea10c7292388623ae951f97f57d
Khoi Ngo <davidkngo@outlook.com>
2023-06-09 18:10:47 +0700

    add custom validation for ibft transaction

--- config/src/main/java/org/hyperledger/besu/config/GenesisConfigOptions.java
+++ config/src/main/java/org/hyperledger/besu/config/GenesisConfigOptions.java
@@ -109,6 +109,9 @@ public interface GenesisConfigOptions {
    */
   QbftConfigOptions getQbftConfigOptions();
 
+  CustomIbftConfigOptions getCustomIbftConfigOptions();
+
+
   /**
    * Gets discovery options.
    *
