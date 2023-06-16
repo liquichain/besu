1bbb417fc09fda1f50d68beb3f063be108377289
Khoi Ngo <davidkngo@outlook.com>
2023-06-16 17:01:09 +0700

    fix: transaction validation

--- consensus/ibft/src/main/java/org/hyperledger/besu/consensus/ibft/LiquichainIBFTProtocolSpecs.java
+++ consensus/ibft/src/main/java/org/hyperledger/besu/consensus/ibft/LiquichainIBFTProtocolSpecs.java
@@ -1,5 +1,10 @@
 package org.hyperledger.besu.consensus.ibft;
 
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSpecBuilder;
+
 public class LiquichainIBFTProtocolSpecs {
 
+  public static ProtocolSpecBuilder frontierDefinition(final ProtocolSpecBuilder builder) {
+    return builder;
+  }
 }
