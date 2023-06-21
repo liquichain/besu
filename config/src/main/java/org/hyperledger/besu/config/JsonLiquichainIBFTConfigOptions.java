df8c81d6900ed5b8c90ee0f9f54a1a357057bd78
Khoi Ngo <davidkngo@outlook.com>
2023-06-21 14:21:22 +0700

    chore: rename allowlist to smartcontract whitelist and add blacklist

--- config/src/main/java/org/hyperledger/besu/config/JsonLiquichainIBFTConfigOptions.java
+++ config/src/main/java/org/hyperledger/besu/config/JsonLiquichainIBFTConfigOptions.java
@@ -15,9 +15,10 @@ public class JsonLiquichainIBFTConfigOptions extends JsonBftConfigOptions implem
 
   //  private static final Logger LOG =
 //      LoggerFactory.getLogger(JsonCustomIbftConfigOptions.class);
-  private static final String CONTRACT_ADDRESSES_KEY = "contractaddresses";
+  private static final String SMART_CONTRACT_WHITELIST_KEY = "smartcontractwhitelist";
+  private static final String SMART_CONTRACT_BLACKLIST_KEY = "smartcontractblacklist";
+
   private final ObjectMapper mapper;
-  private List<String> allowList;
 
   /**
    * Instantiates a new Json bft config options.
@@ -30,20 +31,35 @@ public class JsonLiquichainIBFTConfigOptions extends JsonBftConfigOptions implem
   }
 
   @Override
-  public List<String> getAllowListContractAddresses() {
-    Optional<ArrayNode> maybeNode = JsonUtil.getArrayNode(this.bftConfigRoot, CONTRACT_ADDRESSES_KEY);
+  public List<String> getSmartContractWhiteList() {
+    Optional<ArrayNode> maybeNode = JsonUtil.getArrayNode(this.bftConfigRoot, SMART_CONTRACT_WHITELIST_KEY);
 
+    List<String> whiteList = new ArrayList<>();
 
     try {
-      if (maybeNode.isEmpty()) {
-        allowList = new ArrayList<>();
-      } else {
-        allowList = mapper.treeToValue(maybeNode.get(), mapper.getTypeFactory().constructCollectionType(List.class, String.class));
+      if (maybeNode.isPresent()) {
+        whiteList = mapper.treeToValue(maybeNode.get(), mapper.getTypeFactory().constructCollectionType(List.class, String.class));
       }
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
 
-    return allowList;
+    return whiteList;
   }
+
+  @Override
+  public List<String> getSmartContractBlackList() {
+    Optional<ArrayNode> maybeNode = JsonUtil.getArrayNode(this.bftConfigRoot, SMART_CONTRACT_BLACKLIST_KEY);
+    List<String> blackList = new ArrayList<>();
+    try {
+      if (maybeNode.isPresent()) {
+        blackList = mapper.treeToValue(maybeNode.get(), mapper.getTypeFactory().constructCollectionType(List.class, String.class));
+      }
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+
+    return blackList;
+  }
+
 }
